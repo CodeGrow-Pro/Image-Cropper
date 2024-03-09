f32ed174a838e0ee1dead8d4c37e3d883b6a4bcd
CodeGrow-Pro <vk4993102@gmail.com>
2024-03-09 08:56:47 +0530

    first commit

---
+++ build/precache-manifest.d1a6c5eecdef910ad85c1c9d04ab303b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9334b92ae9aff7304a2c9324b9fab5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0aab2ff6049b3e2207db",
+    "url": "/static/css/main.acc9b15e.chunk.css"
+  },
+  {
+    "revision": "ed06462714f6c64ad095",
+    "url": "/static/js/2.727e5fd8.chunk.js"
+  },
+  {
+    "revision": "0d1578f2c279bbd8fe28",
+    "url": "/static/js/3.0693247c.chunk.js"
+  },
+  {
+    "revision": "38bb489e2c545ea81b9e",
+    "url": "/static/js/4.239bd991.chunk.js"
+  },
+  {
+    "revision": "0ce2c706672f68a703a2",
+    "url": "/static/js/5.3d9977f0.chunk.js"
+  },
+  {
+    "revision": "0aab2ff6049b3e2207db",
+    "url": "/static/js/main.c8c9cc35.chunk.js"
+  },
+  {
+    "revision": "96a13ea567545df158b6",
+    "url": "/static/js/runtime~main.c24f05d3.js"
+  }
+]);
